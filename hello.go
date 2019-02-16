35e358b229ae38b01b5c205ba393aebec193b971
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-16 13:43:13 +0900

    エクスポート - インポート

--- hello.go
+++ hello.go
@@ -5,3 +5,19 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Go!!")
 }
+
+/*
+
+// helloパッケージに所属
+package hello
+
+// 別パッケージから使用できる関数名
+func Hello() {
+	// ...
+}
+
+// 別パッケージから使用できない関数名
+func hello() {
+	// ...
+}
+*/
